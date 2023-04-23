a1f9679967a0b3ee7ce4dfb27fa4ee433d334401
abdo-355 <abdoluxor366@gmail.com>
2023-04-24 01:05:49 +0200

    add website description

--- app/head.tsx
+++ app/head.tsx
@@ -1,6 +1,7 @@
 const Head = () => (
     <>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Metaverse is a new thing in the future, where you can enjoy the virtual world by feeling like it's really real, you can feel what you feel in this metaverse world, because this is really the madness of the metaverse of today, using only VR devices you can easily explore the metaverse world you want, turn your dreams into reality. Let's explore the madness of the metaverse by scrolling down" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
     </>
